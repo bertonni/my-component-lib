6802921f0422161849b059d2b00ef12686486564
bertonni <bertonnipaz@gmail.com>
2025-07-23 11:38:08 -0300

    fix format file on build

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       entry: "./src/index.ts",
       name: "MyGreatComponentLib",
       fileName: "my-great-component-lib",
-      formats: ["cjs"],
+      formats: ["es", "cjs"],
     },
     rollupOptions: {
       external: ["react", "react-dom"],
